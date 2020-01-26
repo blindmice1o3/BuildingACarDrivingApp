692fcdfd51a7f170a0f1f1a4ec7271c82285742b
jamesgov <blindmice1o3@gmail.com>
2020-01-26 15:01:42 -0800

    initial commit of BuildingACarDrivingApp project (to learn Unity), following a Udemy course titled "ILearn Unity For Absolute Beginners".

---
+++ Assets/Scripts/playHorn.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playHorn : MonoBehaviour
+{
+
+    AudioSource audio;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();    
+    }
+
+    public void beep() {
+        audio.Play();
+    }
+    
+}
